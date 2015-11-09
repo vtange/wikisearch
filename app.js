7123ef4f70d1caa38531eda8b68c8351509f0d24
Victor Tang <victortang1992@gmail.com>
2015-11-08 22:02:23 -0800

    returns with titles and snippet

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
   var baseUrl = "https://en.wikipedia.org/w/api.php";
   var query = "?action=query";
   var format = "&format=json";
-  var title = "&titles=";
+  var generator = "&list=search&srsearch=";
+  var utf = "&utf8=";
   var callback = "&callback=JSON_CALLBACK"
     $scope.returns = []
     $scope.searchWiki = function(term){
                                                 $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-                                                $http.jsonp(baseUrl + query + format + title + term + callback).success(function(data) {
+                                                $http.jsonp(baseUrl + query + format + generator + term + callback).success(function(data) {
                                                     // you can do some processing here
                                                      $scope.returns = data;
                                                     console.log($scope.returns);
@@ -27,6 +28,7 @@ app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
                                                     // you can do some processing here
                                                     console.log('error');
                                                 });
+                                        $scope.wikiThis = "";
     }
 }]);//end of controller
   //end of function
