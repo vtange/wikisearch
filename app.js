2201716e42faa9ab6265c4b2aa9a01a2b2759323
Victor Tang <victortang1992@gmail.com>
2015-11-08 20:01:20 -0800

    angular, add "x"

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
     //start of function
   var app = angular.module('WikiSearch', []);
 
-app.factory('memory', function($http){
+app.factory('memory', function(){
+
+  var storage = {};
+  storage.datadata = [];
+  return storage;
+});//end of service
+
+app.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
 
   var baseUrl = "https://en.wikipedia.org/w/api.php";
   var query = "?action=query";
   var format = "&format=json";
   var title = "&titles=";
   var callback = "&callback=JSON_CALLBACK"
-  var storage = {};
-  storage.datadata = [];
+
+    $scope.searchWiki = function(term){
                                                 $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
                                                 $http.jsonp(baseUrl + query + format + title + "san francisco" + callback).success(function(data) {
                                                     // you can do some processing here
@@ -20,13 +27,7 @@ app.factory('memory', function($http){
                                                     // you can do some processing here
                                                     console.log('error');
                                                 });
-
-  return storage;
-});//end of service
-
-app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
-    $scope.storage = memory; // load service
-
+    }
 }]);//end of controller
   //end of function
 })();
